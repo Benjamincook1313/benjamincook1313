2c86f54b294e95e70654ce2c816886994770105a
Benjamin <benjamincook1313@gmail.com>
2020-03-26 17:51:14 -0600

    styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [turn, setTurn] = useState(true)
+  const [boxes, setBoxes] = useState([])
+
+  function turnToggler(){
+    if(turn){
+      return 'O'
+    }
+    return 'X'
+  }
+
   return (
     <div className="App">
-      
+      <div className='top-bar'>
+        <div className='top-item'>{turn? "O's": "X's"} Turn</div>
+        <button className='top-item'>Reset</button>
+      </div>
+      <div className='gamebox'>
+        <div className='left'></div>
+        <div className='right'></div>
+        <div className='top'></div>
+        <div className='bottom'></div>
+      </div>
     </div>
   );
 }
