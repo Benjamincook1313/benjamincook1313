0d93d38adac33131100d4f802ed600881347df71
Benjamin <benjamincook1313@gmail.com>
2020-03-26 18:18:14 -0600

    finished styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
 
   const [turn, setTurn] = useState(true)
-  const [boxes, setBoxes] = useState([])
+  const [boxes, setBoxes] = useState([1,2,3,4,5,6,7,8,9])
 
   function turnToggler(){
     if(turn){
@@ -20,10 +20,15 @@ function App() {
         <button className='top-item'>Reset</button>
       </div>
       <div className='gamebox'>
-        <div className='left'></div>
-        <div className='right'></div>
-        <div className='top'></div>
-        <div className='bottom'></div>
+  <div className='box one'>{boxes[0]}</div>
+        <div className='box two'>{boxes[1]}</div>
+        <div className='box three'>{boxes[2]}</div>
+        <div className='box four'>{boxes[3]}</div>
+        <div className='box five'>{boxes[4]}</div>
+        <div className='box six'>{boxes[5]}</div>
+        <div className='box seven'>{boxes[6]}</div>
+        <div className='box eight'>{boxes[7]}</div>
+        <div className='box nine'>{boxes[8]}</div>
       </div>
     </div>
   );
