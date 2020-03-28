ddd55baddd4d9109b0aed99a20a06ec2a6e2c116
Benjamin <benjamincook1313@gmail.com>
2020-03-27 21:40:29 -0600

    box click function

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 
 function App() {
 
-  const [turn, setTurn] = useState(true)
-  const [boxes, setBoxes] = useState([1,2,3,4,5,6,7,8,9])
+  const [turn, setTurn] = useState('O')
+  const [boxes, setBoxes] = useState(['','','','','','','','',''])
 
-  function turnToggler(){
-    if(turn){
-      return 'O'
+  function boxClicked(i){
+    let insert = boxes.map().splice(i, 0, [turn])
+    if(boxes[i]){
+      window.alert('choose different box')
+    }else{
+      (turn === 'X')? setTurn('O'): setTurn('X')
+      setBoxes(insert)
     }
-    return 'X'
   }
 
   return (
@@ -20,15 +23,15 @@ function App() {
         <button className='top-item'>Reset</button>
       </div>
       <div className='gamebox'>
-  <div className='box one'>{boxes[0]}</div>
-        <div className='box two'>{boxes[1]}</div>
-        <div className='box three'>{boxes[2]}</div>
-        <div className='box four'>{boxes[3]}</div>
-        <div className='box five'>{boxes[4]}</div>
-        <div className='box six'>{boxes[5]}</div>
-        <div className='box seven'>{boxes[6]}</div>
-        <div className='box eight'>{boxes[7]}</div>
-        <div className='box nine'>{boxes[8]}</div>
+      <div className='box one' onClick={() => boxClicked('0')}>{boxes[0]}</div>
+        <div className='box two' id='1' >{boxes[1]}</div>
+        <div className='box three' id='2'>{boxes[2]}</div>
+        <div className='box four' id='3'>{boxes[3]}</div>
+        <div className='box five' id='4'>{boxes[4]}</div>
+        <div className='box six' id='5'>{boxes[5]}</div>
+        <div className='box seven' id='6'>{boxes[6]}</div>
+        <div className='box eight' id='7'>{boxes[7]}</div>
+        <div className='box nine' id='8'>{boxes[8]}</div>
       </div>
     </div>
   );
