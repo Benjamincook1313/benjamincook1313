8b788cb13f452ecddf1d3fa35316e29e9807b3e7
Benjamin <benjamincook1313@gmail.com>
2020-03-31 20:33:21 -0600

    got the checker fuction working

--- src/App.js
+++ src/App.js
@@ -4,49 +4,47 @@ import './App.css';
 function App() {
 
 
-  const [turn, setTurn] = useState('O')
-  const [boxes, setBoxes] = useState([])
+  const [turn, setTurn] = useState("O's Turn")
+  const [boxes, setBoxes] = useState([ '', '', '', '', '', '', '', '', '',])
   const [counter, setCounter] = useState(1)
   
   const checker=()=>{
-    let b = boxes
+    const b = boxes
     if(b[0] !== '' && b[0] === b[1] && b[0] === b[2]){
       setTurn(`${b[0]} Wins`)
-    }
-    if(b[0] !== '' && b[3] === b[4] && b[3] === b[5]){
+    }else if(b[3] !== '' && b[3] === b[4] && b[3] === b[5]){
       setTurn(`${b[3]} Wins`)
-    }
-    if(b[0] !== '' && b[6] === b[7] && b[6] === b[8]){
+    }else if(b[6] !== '' && b[6] === b[7] && b[6] === b[8]){
       setTurn(`${b[6]} Wins`)
-    }
-    if(b[0] !== '' && b[0] === b[4] && b[0] === b[8]){
+    }else if(b[0] !== '' && b[0] === b[3] && b[0] === b[6]){
       setTurn(`${b[0]} Wins`)
-    }
-    if(b[0] !== '' && b[2] === b[4] && b[2] === b[6]){
-      setTurn(`${b[2]} Wins`)
-    } 
-    if(b[0] !== '' && b[0] === b[3] && b[0] === b[6]){
-      setTurn(`${b[0]} Wins`)
-    } 
-    if(b[0] !== '' && b[1] === b[4] && b[2] === b[7]){
+    }else if(b[1] !== '' && b[1] === b[4] && b[1] === b[7]){
       setTurn(`${b[1]} Wins`)
-    } 
-    if(b[0] !== '' && b[2] === b[5] && b[2] === b[8]){
+    }else if(b[2] !== '' && b[2] === b[5] && b[2] === b[8]){
       setTurn(`${b[2]} Wins`)
-    }
-    setTurn('Cats Game')
+    }else if(b[0] !== '' && b[0] === b[4] && b[0] === b[8]){
+      setTurn(`${b[0]} Wins`)
+    }else if(b[2] !== '' && b[2] === b[4] && b[2] === b[6]){
+      setTurn(`${b[2]} Wins`)
+    }else if(counter === 9){
+      setTurn('Cats Game')
+    }else{
+      return 
+    } 
   };
   
   const boxClicked=(i)=>{
     let insert = [...boxes]
-    if(boxes[i] === 'O' || boxes[i] === 'X'){
+    if(boxes[i] === "O's Turn" || boxes[i] === "X's Turn"){
       return window.alert('choose a different box')
     }else{
       insert[i] = turn
       setBoxes(insert)
       setCounter(counter+1)
     };
-    turn === 'X'?setTurn('O'):setTurn('X')
+    if(counter !== 9){
+      turn === "X's Turn"?setTurn("O's Turn"):setTurn("X's Turn")
+    }
     console.log(counter)
     checker()
   };
@@ -54,19 +52,19 @@ function App() {
   return (
     <div className="App">
       <div className='top-bar'>
-        <div className='top-item'>{`${turn}'s`} Turn</div>
-        <button className='top-item' onClick={() => setBoxes([])/setCounter(1)}>Reset</button>
+        <div className='top-item'>{`${turn}`}</div>
+        <button className='top-item' onClick={() => setBoxes([ '', '', '', '', '', '', '', '', '',])/setCounter(1)}>Reset</button>
       </div>
       <div className='gamebox'>
-      <div className='box one' onClick={() => boxClicked('0')}>{boxes[0]}</div>
-        <div className='box two'onClick={() => boxClicked('1')}>{boxes[1]}</div>
-        <div className='box three' onClick={() => boxClicked('2')}>{boxes[2]}</div>
-        <div className='box four' onClick={() => boxClicked('3')}>{boxes[3]}</div>
-        <div className='box five' onClick={() => boxClicked('4')}>{boxes[4]}</div>
-        <div className='box six' onClick={() => boxClicked('5')}>{boxes[5]}</div>
-        <div className='box seven' onClick={() => boxClicked('6')}>{boxes[6]}</div>
-        <div className='box eight' onClick={() => boxClicked('7')}>{boxes[7]}</div>
-        <div className='box nine' onClick={() => boxClicked('8')}>{boxes[8]}</div>
+      <div className='box one' onClick={() => boxClicked('0')}>{boxes[0].split("'").shift()}</div>
+        <div className='box two'onClick={() => boxClicked('1')}>{boxes[1].split("'").shift()}</div>
+        <div className='box three' onClick={() => boxClicked('2')}>{boxes[2].split("'").shift()}</div>
+        <div className='box four' onClick={() => boxClicked('3')}>{boxes[3].split("'").shift()}</div>
+        <div className='box five' onClick={() => boxClicked('4')}>{boxes[4].split("'").shift()}</div>
+        <div className='box six' onClick={() => boxClicked('5')}>{boxes[5].split("'").shift()}</div>
+        <div className='box seven' onClick={() => boxClicked('6')}>{boxes[6].split("'").shift()}</div>
+        <div className='box eight' onClick={() => boxClicked('7')}>{boxes[7].split("'").shift()}</div>
+        <div className='box nine' onClick={() => boxClicked('8')}>{boxes[8].split("'").shift()}</div>
       </div>
     </div>
   );
