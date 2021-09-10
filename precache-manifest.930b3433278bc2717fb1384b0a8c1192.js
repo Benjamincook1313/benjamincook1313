528b2f37f12469a5d93e2f0a88ccb15e92b46b36
Benjamin <benjamincook1313@gmail.com>
2021-09-10 17:07:11 -0600

    Updates

---
+++ precache-manifest.930b3433278bc2717fb1384b0a8c1192.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea2e0e55ccbdedf28b7169acb8432e6",
+    "url": "/benjamincook1313/index.html"
+  },
+  {
+    "revision": "631d2a44f72b73334a33",
+    "url": "/benjamincook1313/static/css/main.3afec8bf.chunk.css"
+  },
+  {
+    "revision": "a9915dbf1968f0f46136",
+    "url": "/benjamincook1313/static/js/2.3aaf8356.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/benjamincook1313/static/js/2.3aaf8356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631d2a44f72b73334a33",
+    "url": "/benjamincook1313/static/js/main.f7b863b4.chunk.js"
+  },
+  {
+    "revision": "34c6bd6fd7455d4197b6",
+    "url": "/benjamincook1313/static/js/runtime-main.3d4bd64e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Benjamincook1313/Benjamincook1313/precache-manifest.84d3159842acfdf98081c705ebf3758e.js"
+  "/benjamincook1313/precache-manifest.930b3433278bc2717fb1384b0a8c1192.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Benjamincook1313/Benjamincook1313/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/benjamincook1313/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
